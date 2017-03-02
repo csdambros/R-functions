44fb8daff7d5d20fc232e251d1fb36f66719ae27
Cristian <csdambros@gmail.com>
2017-03-02 19:01:08 -0300

    Update poncho.R

--- poncho.R
+++ poncho.R
@@ -1,188 +1,130 @@
 ####################################################
 #Function to plot histograms of species distributions along environmental gradients 
-#CSDambros November-03-2012 (latest version)
-#Version 3.2
+#CSDambros July-06-2014 (latest version)
+#Version 4.0
 
 #################################
-#Use: Poncho (table,gradient=NULL,col=1,col.places=2,col.species=3,
-#		col.gradient="gray",highlight.species=NA,highlight.places=NA,
-#		xyratio=1.3333333333,cex.species=NULL,lwd=1,
-#		cex.lab=NULL,cex.title=3,cex.gradient=2,
-#		file=NULL,title=NULL,xlab="Ordered places",
-#		ylab="Abundance",show.grad=NULL,show.places=NULL,
-#		xfactor=0,lty=1,add=F,spgradient=NULL)
+#Use: 
+
+#		poncho (x,env=NULL,phy=NULL,col=1,col.gradient="gray",
+#		,cex.species=0.6,cex.lab=NULL,cex.gradient=2,
+#		file=NULL,xlab="Ordered places",ylab.top="Abundance",
+#		ylab.bottom="Environment",lty=3)
 
 #alternativelly try:
 
-#      Poncho3.2 (table)
+#      poncho (x)
 
 
 #Parameters:
 
-# table			a numeric matrix, data frame or object containing objects in rows and atributes in columns.
-	# gradient			an optional vector containing a atribute to order the table
-	# col				the initial color(s) to fill or shade the rectangle(s) with. The default 1 means "black"
-	# col.places		the color to fill or shade the species rectangles with. Only changes if highlight.species is not NULL
-	# col.species		the color to fill or shade the places rectangles with. Only changes if highlight.places is not NULL
-	# col.gradient  		the color to fill or shade the gradient rectangles with. Only changes if show.grad is not NULL
-	# highlight.species	a vector containing the name of species that should be highlighted. The names should be the same as in table colnames 
-	# highlight.places	a vector containing the name of places that should be highlighted.The names should be the same as in table rownames 
-	# xyratio			xy ratio. The default value is 1.33. Changing this factor make graph more or less rectangular
-	# cex.species		The magnification to be used for the name of atributes relative to the current setting of cex
-	# lwd				The line width, a positive number, default is 1
-	# cex.lab			The magnification to be used for x and y labels relative to the current setting of cex
-	# cex.title			The magnification to be used for main title relative to the current setting of cex
-	# cex.gradient		The magnification to be used for gradient name relative to the current setting of cex
-	# file			The name of the output file, up to 511 characters. Should have a .pdf at the end name. If is NULL the image is plotted and not saved as pdf file
-	# title			The main title (on top)
-	# xlab			Label for the x axis
-	# ylab			Label for the y axis
-	# gradlab			label for the gradient name
-	# show.grad			If not NULL, do not show the gradient, default is TRUE
-	# show.places		If not NULL, show ordered places names as in table rownames 
-	# xfactor			Add space on the graph to suport longer species names
-	# spgradient		If not NULL, provide the order of species position
+# x			a numeric matrix, data frame or object containing objects in rows and atributes in columns.
+# env		an optional vector containing a atribute to order the table
+# phy		Optional; Phylogenetic tree to order the species and plot.
+# col		The initial color(s) to fill or shade the rectangle(s). The default 1 means "black". To color the species, provide a vector with the color (or factor) for each species. To color by the sites, provide a vector with the color for each site. To individually color each cell, provide a matrix with the same dimensions as x (main table). The function will recognize the length of the color vector and automatically attribute to sites, species or both.
+# col.gradient  		the color to fill or shade the gradient rectangles with. Only changes if show.grad is not NULL
+# cex.species		The magnification to be used for the name of atributes relative to the current setting of cex
+# cex.lab			The magnification to be used for x and y labels relative to the current setting of cex
+# cex.title			The magnification to be used for main title relative to the current setting of cex
+# cex.gradient		The magnification to be used for gradient name relative to the current setting of cex
+# file			The name of the output file, up to 511 characters. Should have a .pdf at the end name. If is NULL the image is plotted and not saved as pdf file
+# xlab			Label for the x axis
+# ylab.top			Label for the y axis
+# ylab.bottom			label for the gradient name
 
 #################################
 #Function
 
-poncho<-function(
-table,col=1,col.places=2,col.species=3,col.gradient="gray",
-highlight.species=NA,highlight.places=NA,
-xyratio=1.3333333333,cex.species=NULL,lwd=1,
-cex.lab=NULL,cex.title=3,cex.gradient=2,gradient=NULL,
-file=NULL,title=NULL,xlab="Ordered places",
-ylab="Abundance",show.grad=TRUE,show.places=NULL,
-xfactor=0,gradlab="Gradient",lty=1,lty.lines=1,border=1,decrease=FALSE,add=F,spgradient=NULL)
-{
+poncho<-function(x,env=gradient,col=col.places,border="grey",col.gradient="grey",cex.species=0.5,cex.lab=2,cex.gradient=1,file=NULL,xlab="Ordered sites",ylab.top=ylab,ylab="Abundance",ylab.bottom=gradlab,lty.lines=3,decrease=FALSE,asis=FALSE,phy=NULL,symbol=1,lty=3,col.lty="darkgrey",col.places=1,col.species=1,gradlab="Environment",gradient=NULL){
+	
+	symbol<-((symbol-1)%%2)+1
+	col.orig<-col
+	col<-as.integer(as.factor(col))
+	
+	if(is.null(env)){env<-prcomp(x)$x[,1]}
+	if(length(col)==ncol(x)){z<-(t(t(x>0)*as.integer(col)))}else{
+		z<-(x>0)*as.integer(col)}
+	
+	z<-z[order(env),order(colSums(x*env)/colSums(x))]
+	
+	if(is.null(phy)){
+				}else{
+#					prephy<-cophenetic(phy)
+#					phy<-di2multi(as.phylo(hclust(as.dist(prephy[match(colnames(z),colnames(prephy)),match(colnames(z),colnames(prephy))]))))
+				  phy<-rotateConstr(phy,colnames(z))
+				  
+				  z<-z[,match(phy$tip.label[phy$edge[,2][phy$edge[,2]<=length(phy$tip.label)]],colnames(z))];
+			colnames(z)<-phy$tip.label[phy$edge[,2][phy$edge[,2]<=length(phy$tip.label)]]
+				}
+	
+	
+	
+	space.x=(0.70)/(nrow(z)+1)
+	space.y=1/(ncol(z))
+	
+	x1=((z>0)*seq(.1,.8-space.x,length=nrow(z)))[(!is.na(z))&z>0]
+	y1=(t(t(z>0)*(seq(0,1-space.y,length=ncol(z)))))[(!is.na(z))&z>0]
+	
+	# Plot the figure
+	
+	op<-par(no.readonly=T)
+	
+	par(mar=c(0,0,0,0),oma=c(0,0,0,0),xpd=NA)
+	layout(matrix(c(1,1,1,1,2),5,1))
+	
+	xlim=c(0,1)
+	ylim=c(0,1)
+	
+	if(!is.null(phy)){
+		require(ape)
+		layout(matrix(c(1,1,1,1,0,2,2,2,2,3,2,2,2,2,3),5,3))
+		plot(phy, show.tip.label=F,plot=TRUE,root.edge = TRUE,no.margin = TRUE)
+		ylab.top=""
+		xlim=c(0.15,1.1);ylim=c(space.y/2,1-space.y/2)
+	
+		}
+	
+	plot.new()
+	plot.window(xlim=xlim,ylim=ylim)
+	
+	segments(0.09,seq(space.y/2,1-space.y/2,length=ncol(z)),0.81,seq(space.y/2,1-space.y/2,length=ncol(z)),lty=lty,col=col.lty)
+	
+	segments(c(0,.09,.09),c(-.02,-0.01,-0.01),c(.82,.82,.09),c(-.02,-.01,1),lty=c(3,1,1))
+	text(.02,0.5,ylab.top,srt=90,cex=2)
+	
+	if(symbol==1){rect(x1,y1,x1+space.x,y1+space.y,col=col.orig[z[(!is.na(z))&z>0]],border = border)}
+	if(symbol==2){points(x1+space.x/2,y1+space.y/2,pch=21,col=border,bg=col.orig[z[(!is.na(z))&z>0]])}
+	
+	text(.82,seq(space.y/2,1-space.y/2,length=ncol(z)),gsub("_"," ",colnames(z)),adj=0,cex=cex.species,col=col.species,font=3)
+	
+	# Plot gradient
+	
+	plot.new()
+	plot.window(xlim=xlim,ylim=c(-.5,1.2))
+	
+	gradient<-sort(env,decreasing=F)
+	gradient2<-gradient/abs(max(c(gradient,0))-min(c(gradient,0)))
+	val=-min(c(gradient2,0))
+	
+	segments(c(.085,.085,.085,.1,.1,.8,.1),c(0,0,1,-.05,-.05,-.05,val),c(.075,.085,.075,.8,.1,.8,.8),c(0,1,1,-.05,-.1,-.1,val))
+	
+	rect(seq(.1,0.8-space.x,length=nrow(z)),val,seq(.1,.8-space.x,length=nrow(z))+space.x,val+gradient2,col=col.gradient)
+	
+	text(.065,c(0,1),c(floor(min(gradient)),ceiling(max(gradient))),adj=1,cex=.8)
+	text(.8-(.8-.1)/2,-.35,xlab,cex=2)
+	text(.01,0.5,ylab.bottom,srt=90)
+	
+	# Print to pdf device
+	
+	if(!is.null(file)){
+		dev.copy(pdf,file=paste(file,rep(".pdf",1-length(grep(".pdf",file))),sep=""),width=5+(!is.null(phy))*1.5,height=8.27)
+		dev.off()
+	}else{cat('use file = "filename.pdf" to save as a pdf')}
 
-if(is.null(file)){
-if(is.null(cex.species)){cex.species=.5}
-if(is.null(cex.lab)){cex.lab=2}
-if(is.null(cex.gradient)){cex.gradient=1.5}
-}
-
-if(is.null(cex.gradient)){cex.gradient=2}
-if(is.null(cex.lab)){cex.lab=3}
-
-####################################
-
-if(is.null(file)==FALSE){
-
-pdf(file=file,,paper="A4",width=0,height=0)
-
-}
-
-par(mar=c(2,3,.8,3))
-
-#################################
-
-if(is.null(gradient)){gradient<-prcomp(table)$x[,1]}
-
-multiplier=ifelse(decrease==F,1,-1)
-
-
-if(is.null(spgradient)==F){
-
-tabela<-table[order(multiplier*gradient),order(spgradient)]
-if(length(highlight.species)==length(col.species)){
-col.species<-col.species[order(spgradient)]
-}
-}else{
-
-tabela<-table[order(multiplier*gradient),order(colSums(table*multiplier*gradient)/colSums(table))]
-
-if(length(highlight.species)==length(col.species)){
-col.species<-col.species[order(colSums(table*multiplier*gradient)/colSums(table))]
-}
-
-
-}
-
-
-#################################
-x<-90
-y<-x*xyratio
-
-dimx<-x/length(0:(nrow(tabela)-1))
-dimy<-y/length(1:ncol(tabela))
-showg=0
-if(is.null(show.grad)==F){showg=22}
-
-if(add==F){
-plot.new()
-plot.window(xlim=c(0,120+xfactor),ylim=c(0,120+showg))
-}
-
-col<-rep(col,nrow(tabela))
-col[match(highlight.places,rownames(tabela))]<-col.places
-
-for(i in 1:ncol(tabela)){
-
-col.sp<-match(highlight.species,colnames(tabela)[i])
-
-if(sum(col.sp,na.rm=T)!=0){col.2=col.species}else{col.2=col}
-
-if(length(highlight.species)==length(col.species)){col.2=col.species[i]}
-
-rect((0:(nrow(tabela)-1))*dimx,(i-1)*dimy,(1:nrow(tabela))*dimx-dimx*.1,(i-1)*dimy+(tabela[,i]/max(tabela))*dimy*.8,col=col.2,lty=lty,lwd=lwd,border=border)
-lines(c(-1,(nrow(tabela))*dimx),c((i-1)*dimy,(i-1)*dimy),lty=lty.lines,lwd=lwd)
-}
-text(rep(x+2,ncol(tabela)),cumsum(rep(120/ncol(tabela),ncol(tabela)))-120/ncol(tabela)/2,gsub("_"," ",colnames(tabela)),cex=if(is.null(cex.species)){ifelse(30/ncol(tabela)>1,1,30/ncol(tabela))}else{cex.species},adj=0,font=3)
-
-if(lty.lines!=0){
-rect(-1,0,-1,(ncol(tabela)-1)*dimy+dimy*.8,lwd=lwd)
-}
-
-text(x+4,133.5,gradlab,cex=cex.gradient,adj=0)
-
-par(mgp=c(1,4,5))
-title(main=title,cex.main=cex.title)
-
-par(mgp=c(0,4,5))
-title(ylab=ylab,cex.lab=cex.lab)
-
-par(mgp=c(1,8,10),mar=c(3.5,3,.8,8))
-title(xlab=xlab,cex.lab=cex.lab)
-
-
-par(mgp=c(4,8,10),mar=c(0,0,0,0))
-if(is.null(show.grad)==F){
-gradient<-sort(gradient,decreasing=decrease)
-gradient2<-gradient/abs(max(c(gradient,0))-min(c(gradient,0)))
-val=126-min(c(gradient2*15,0))
-
-rect((0:(nrow(tabela)-1))*dimx,val,(1:nrow(tabela))*dimx-dimx*.1,val+gradient2*15,lwd=lwd,col=col.gradient)
-
-rect(x+2,126,x+2,126+15,lwd=lwd)
-
-rect(x+2,126,x+3,126,lwd=lwd)
-
-rect(x+2,126+15,x+3,126+15,lwd=lwd)
-
-text(x+3.5,126,round(min(c(0,gradient)),2),adj=0,cex=.7)
-text(x+3.5,126+15,round(max(c(0,gradient)),2),adj=0,cex=.7)
-
-}
-
-par(mar=c(5, 4, 4, 2) + 0.1,mgp=c(0,-.5,0))
-if(is.null(show.places)==F){
-axis(1,at=((0:(nrow(tabela)-1))*dimx+(1:nrow(tabela))*dimx-dimx*.1)/2,labels=rownames(tabela),las=2,cex.axis=if(is.null(cex.species)){ifelse(30/ncol(tabela)>1,1,30/ncol(tabela))}else{cex.species},tick=F)
-}
-#title(main="B",adj=0,cex.main=2)
-par(mar=c(5, 4, 4, 2) + 0.1,mgp=c(3, 1, 0))
-if(is.null(file)==F){
-
-dev.off()
-paste("You have saved the image as",file)
-}else{"If resolution is low, try to use the argument file=imagename.pdf to save the image in a file"}
+	par(op)
 
 }
 
 
-#end
-
-##Use
-#poncho(table)
 
 
