77d1febe3187ed196f4c6c6ba40fe9344d5caa39
Cristian <csdambros@gmail.com>
2021-01-14 10:23:15 -0300

    Update plight.R

--- plight.R
+++ plight.R
@@ -327,6 +327,249 @@ for (i in 1:length(pics)){
 
 
 
+require(raster)
+
+
+select.pic<-function(file="."){
+
+  pics<-basename(file)
+
+  cat(paste0("(",1:length(pics),") ",pics),sep = "\n")
+  
+  ref <- readline("Type picture reference number (training):")
+  cat("File",file[as.integer(ref)],"selected\n")
+
+  return(file[as.integer(ref)])
+  
+}
+
+
+select.region<-function(raster,plot.new=TRUE,col=2,density=20,maxpixels=10000000,...){
+
+  if(plot.new){
+    
+    plotRGB(brick(raster),maxpixels=maxpixels)
+    
+  }
+  
+  
+values<-list()
+
+extmx<-{}
+extmy<-{}
+
+i=1
+
+continue<-"Y"
+
+while(continue=="Y"){
+  
+  cat("Select a region")
+  x<-"Y"
+  
+  sel<-select(raster)
+  
+  while(!x%in%c("Y","N")){
+    x <- toupper(readline("Keep selection (Y/N)?"))
+    if(!x%in%c("Y","N")){cat("Type Y for Yes or N for No")}
+  }
+  
+  if(x=="Y"){
+    values[[i]]<-getValues(sel)
+    i<-i+1
+    
+    ext<-extent(sel)
+    
+    extmx<-cbind(extmx,ext[c(1,2)])
+    extmy<-cbind(extmy,ext[c(3,4)])
+    
+    rect(extmx[1,],extmy[1,],extmx[2,],extmy[2,],col=col,density = density,...)
+  }
+  
+  continue<-""
+  
+  while(!continue%in%c("Y","N")){
+    continue <- toupper(readline("Continue selecting other regions in this class (Y/N)?"))
+    if(!continue%in%c("Y","N")){cat("Type Y for Yes or N for No")}
+    
+  }
+  
+}
+
+return(values)
+
+}
+
+
+model.train<-function(file=".",nclasses=2,classnames=1:nclasses,maxpixels=500000){
+
+  require(raster)
+  require(tree)
+  
+  if(length(classnames)!=nclasses){
+    
+    stop("classnames length must equal nclasses")
+    
+  }
+  
+  picsref<-file
+  
+  if(!class(file)%in%c("raster","RasterStack","RasterBrick","RasterLayer")){
+    
+  if(dir.exists(file[1])){
+    
+    file<-list.files(file,full.names = TRUE)
+
+  }
+    
+    if(length(file)>1){
+    
+    picsref<-select.pic(file = file)
+    }
+    
+  }
+
+picture<-stack(picsref)
+
+plotRGB(picture,maxpixels=maxpixels)
+
+resu<-list()
+
+for(k in 1:nclasses){
+  cat("Class",classnames[k],"\n")
+  resu[[k]]<-select.region(raster=picture,col=k,plot.new=FALSE)
+}
+
+resu2<-lapply(resu,do.call,what = rbind)
+resu3<-do.call(rbind,resu2)
+
+n<-unlist(lapply(resu2,nrow))
+
+resp<-factor(rep(classnames,n))
+
+data<-data.frame(resp,resu3)
+
+cat("Building model...")
+m1<-tree(resp~.,data=data)
+cat("Done\n")
+
+return(m1)
+
+}
+
+model.classify<-function(m1,file=".",plot=FALSE){
+
+require(raster)
+require(tree)
+
+  picsref<-file
+  
+  if(!class(file)%in%c("raster","RasterStack","RasterBrick","RasterLayer")){
+    
+  if(dir.exists(file[1])){
+    
+    picsref<-list.files(file,full.names = TRUE)
+
+  }
+    
+    pics<-basename(picsref)
+  
+  }else{
+      
+    pics<-filename(file)    
+    }
+  
+cat("Classifying regions in",length(pics),"files\n")
+
+op<-par(no.readonly = TRUE)
+
+ if(plot){
+   par(mfrow=c(1,2))
+ }
+
+resu<-matrix(NA,length(pics),1+nlevels(m1$y))
+rownames(resu)<-pics
+colnames(resu)<-c("npixels",levels(m1$y))
+
+
+for (i in 1:length(pics)){
+
+  file2<-file
+  
+  if(!class(file)%in%c("raster","RasterStack","RasterBrick","RasterLayer")){
+    
+    file2<-stack(picsref[i])
+  }
+  
+  allpic<-getValues(file2)
+  
+  colnames(allpic)<-labels(m1$terms)
+  
+  predicted<-predict(m1,newdata=data.frame(allpic),type="class")
+    
+  n<-length(predicted)
+  predtable<-table(predicted)
+
+  resu[i,]<-c(n,predtable)
+  print(i)
+  
+  if(plot){
+  
+  picturecopy<-raster(file2)
+  picturecopy<-setValues(picturecopy,predicted)
+
+  plotRGB(file2)
+  plotRGB(file2)
+  plot(picturecopy,axes=FALSE,box=FALSE,add=TRUE,col=adjustcolor(c(NA,2:nlevels(predicted))),legend=FALSE)
+  #plot(picturecopy,axes=FALSE,box=FALSE,add=TRUE,legend=FALSE)
+
+  }
+}
+ 
+  par(op)
+  return(resu)
+   
+}
+
+
+# files<-list.files("CanopyCoverData",full.names = TRUE)
+# 
+# ver<-brick(files[1])
+# ver2<-aggregate(ver,fact=6)
+# 
+# m1<-model.train(ver2)
+# 
+# model.classify(m1,ver2,plot=TRUE)
+# 
+
+
+
+# m1<-model.train(file = "../Aula10.MoldenBread",maxpixels = 10000)
+# model.classify(m1,file = "../Aula10.MoldenBread",plot = TRUE)
+# 
+# ver<-brick("../Aula10.MoldenBread/canopy.jpg")
+# ver2<-aggregate(ver,fact=4)
+# 
+# m1<-model.train(ver2,maxpixels = 10000000,nclasses = 3,classnames = c("sol","ceu","arvore"))
+# model.classify(m1,ver2,plot = TRUE)
+# 
+# 
+# getValues(ver2)
+# 
+# 
+# 
+# ver<-brick("../Aula10.MoldenBread/canopy.jpg")
+# 
+# 
+# 
+# 
+# pgreen("../Aula10.MoldenBread/canopy.jpg",showRGB = TRUE)
+# 
+# ver<-stack("../Aula10.MoldenBread/canopy.jpg")
+# ver2<-getValues(ver)
+# 
+# 
+# 
 
 
 
